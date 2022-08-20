1cd9d732bce3b87d73d956b3ae266e2784624b79
taiki1frsy <taiki.furusyo@gmail.com>
2022-08-20 21:15:54 +0900

    cargo fmt

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,7 +3,9 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw_ibc_callback_example::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, IbcExecuteMsg, GetCountResponse};
+use cw_ibc_callback_example::msg::{
+    ExecuteMsg, GetCountResponse, IbcExecuteMsg, InstantiateMsg, QueryMsg,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
